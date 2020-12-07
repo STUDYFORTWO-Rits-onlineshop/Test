a0e6ca72058f43272e9b78753c23dd79296d86c6
unknown <degunao200136@gmail.com>
2020-12-08 02:27:33 +0900

    初めてのコミット

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+  public static void main(String... args) {
+    //【基本課題】自分の学生証番号と名前を出力する．
+    /* ここから */
+    System.out.println("Hello World");
+    System.out.println("私の名前は出口尚哉です。");
+    /* ここまで */
+
+    //【発展課題】あらかじめ用意した複数のメッセージ（テキスト）の中から，一つをランダムに出力する．
+    /* ここから */
+
+
+
+    /* ここまで */
+  }
+}
